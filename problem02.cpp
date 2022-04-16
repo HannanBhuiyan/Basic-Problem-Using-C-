99f61abd950f647b9404ae05c2d895b45e968551
HannanBhuiyan <hannan.diuh@gmail.com>
2022-04-17 04:54:54 +0600

    first upload

---
+++ problem02.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+    // problem :  Print the sum of two numbers
+
+    int x, y;
+    cout << "Enter First number " << endl;
+    cin >> x;
+
+    cout << "Enter Second number " << endl;
+    cin >> y;
+
+    int result = x + y;
+
+    cout << "The sum of " << x << " and " << y << " is  :" << result <<endl;
+
+
+
+return 0;
+}
---
+++ problem04.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+     int a, b, result;
+
+     a = 20;
+     b = 30;
+
+     result = a + b;
+
+     cout << "The sum of " << a << " and " << b << " is :" << result << endl;
+
+
+
+return 0;
+}
